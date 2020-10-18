75749321b9cafc958c7e25227b685612a025230b
y62 <y62@outlook.dk>
2020-10-18 20:03:15 +0200

    API added and finished frontend

--- app.js
+++ app.js
@@ -16,25 +16,43 @@ let stones = [
 ]
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/frontend/home.html");
+   return res.sendFile(__dirname + "/frontend/home.html");
 });
 
 app.get("/basics", (req, res) => {
-    res.sendFile(__dirname + "/frontend/basics.html");
+   return res.sendFile(__dirname + "/frontend/basics.html");
+});
+
+app.get("/express", (req, res) => {
+   return res.sendFile(__dirname + "/frontend/express.html");
+});
+
+app.get("/stone", (req, res) => {
+    return res.sendFile(__dirname + "/frontend/stone_api.html");
 });
 
 app.get("/stones", (req, res) =>  {
  return res.send({data: stones});
 });
 
-app.post("/cars", (req, res) => {
+app.post("/stones", (req, res) => {
    const newStone = req.body;
    stones.push(newStone);
-    return res.send({data: newStone});
+    return res.send({data: req.body});
+});
+
+app.put("/stones/:id", (req, res) => {
+    const newStone = req.body;
+    stones.push(newStone);
+    return res.send({data: req.body});
+});
+
+app.delete("/stones/:id", (req, res) => {
+    stones = stones.filter(stone => stone.id !== Number(req.params.id));
+    return res.send({ data: stones });
 });
 
 
-
 app.listen(port, () => {
-    console.log("Server is running...")
+    console.log("Server is running on port:", port)
 });
